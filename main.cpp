e200b66092ab6e1a63e31c7fa0b18de72c451326
0wl <patakiarpad11@gmail.com>
2026-03-02 23:52:13 +0100

    hibak kijavitva

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //rossz valtozonev
-    std::cout << '1-100 ertekek duplazasa' //hianyzik a vege
-    for (int i = 0;) //hianyzik a feltetel
+    int *b = new int[N_ELEMENTS]; //valtozonev javitva
+    std::cout << "1-100 ertekek duplazasa" << std::endl; //vege hozzaadva
+    for (int i = 0; i < N_ELEMENTS; i++) //feltetel javitva
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) //feltetel javitva
     {
-        std::cout << "Ertek:" //hianyzik a vege
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) //, helyett ;
+    int atlag=0; //alapertek megadva
+    for (int i = 0; i < N_ELEMENTS; i++) //;-re javitva
     {
-        atlag += b[i] //hianyzik a ;
+        atlag += b[i]; //; hozzaadva
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
